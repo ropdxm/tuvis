a2eeee282ecadf575b0495c8dd092ea791f3658f
Dulat <ropdxm@gmail.com>
2026-03-28 23:37:31 +0500

    removed dotenv

--- src/lib/seedFirebase.ts
+++ src/lib/seedFirebase.ts
@@ -8,9 +8,6 @@
 
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import * as dotenv from "dotenv";
-
-dotenv.config({ path: ".env.local" });
 
 initializeApp({
   credential: cert({
